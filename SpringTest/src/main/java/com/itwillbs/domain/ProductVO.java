2069af1a516a23fb9872aa316001b8c0c7769d0f
unknown <rnrudxo2872@naver.com>
2021-06-24 12:58:29 +0900

    update

---
+++ SpringTest/src/main/java/com/itwillbs/domain/ProductVO.java
@@ -0,0 +1,34 @@
+package com.itwillbs.domain;
+
+public class ProductVO {
+	//VO(Value Object) <===> DTO <===> JavaBean
+
+	private String name;
+	private double price;
+	
+	public ProductVO() {
+	}
+	
+	public ProductVO(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductVO [name=" + name + ", price=" + price + "]";
+	}
+}
---
+++ SpringTest/src/main/webapp/WEB-INF/views/productDetail.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>WEB-INF/views/productDetail.jsp</h1>
+컨트롤러에서 전달된 객체 정보 <br>
+<h3>${vo}</h3>
+
+<h4>상품명 : ${vo.name }</h4>
+<h4>가격 : ${vo.price }</h4>
+
+<hr>
+
+<h1>전달되는 속성의 key값이 없을경우 클래스명의 첫글자만 소문자로 변경</h1>
+
+<h2>${productVO }</h2>
+<h4>상품명 : ${productVO.name }</h4>
+<h4>가격 : ${productVO.price }</h4>
+
+</body>
+</html>
